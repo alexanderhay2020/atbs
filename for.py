239710af04113b1b70b863ffcada17eeaf965cb2
alex hay <alexanderhay2020@u.northwestern.edu>
2019-09-15 15:55:05 -0500

    Working through functions portion of ATBS

---
+++ for.py
@@ -0,0 +1,25 @@
+# Working through for loop in ATBS
+
+#print 'my name is '
+#for i in range(5): #initializes i as 0, auto iterates
+#    print 'Jimmy Five Times ' +str(i)
+
+
+# same thing as program befre but with a while loop
+# while loops need definging, for loops self define
+#i=0
+#while i<5:
+#    print 'Jimmy Five Times ' + str(i)
+#    i=i+1
+
+
+# range function - defines where i is initialized
+print 'my name is '
+for i in range(5,10, 2): #initializes i as 5, iterates until 10, steps by 2
+    print 'Jimmy Five Times ' +str(i)
+
+
+#total=0
+#for num in range(101):
+#    total =total + num
+#print total
--- while.py
+++ while.py
@@ -1,6 +1,34 @@
 # This works through the 'while loop' portion of ATBS
 
+#i=0
+#while i < 5:
+#    print 'hello world'
+#    i=i+1
+
+#name=''
+#while name != 'your name':
+#    print 'please type your name'
+#    name=raw_input()
+#print 'thank you'
+
+
+# Using a 'break' command
+# program does same thing as program above
+
+#name=''
+#while True: #starts infinite loop
+#    print 'please type your name'
+#    name=raw_input()
+#    if name=='your name':
+#        break
+#print 'thank you'
+
+
+# Using a 'continue' command
+
 i=0
-while i < 5:
-    print 'hello world'
+while i<5:
     i=i+1
+    if i==3:
+        continue
+    print 'i=' + str(i)
