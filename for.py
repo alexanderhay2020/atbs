5336c0a298e2675ece6f9be25de04719be0b0643
alex hay <alexanderhay2020@u.northwestern.edu>
2019-10-26 12:59:22 -0500

    linted

--- for.py
+++ for.py
@@ -1,25 +1,25 @@
 # Working through for loop in ATBS
 
-#print 'my name is '
-#for i in range(5): #initializes i as 0, auto iterates
+# print 'my name is '
+# for i in range(5): #initializes i as 0, auto iterates
 #    print 'Jimmy Five Times ' +str(i)
 
 
 # same thing as program befre but with a while loop
 # while loops need defining, for loops self define
-#i=0
-#while i<5:
+# i=0
+# while i<5:
 #    print 'Jimmy Five Times ' + str(i)
 #    i=i+1
 
 
 # range function - defines where i is initialized
-#print 'my name is '
-#for i in range(5,10, 2): #initializes i as 5, iterates until 10, steps by 2
+# print 'my name is '
+# for i in range(5,10, 2): #initializes i as 5, iterates until 10, steps by 2
 #    print 'Jimmy Five Times ' +str(i)
 
 # sums 1-100
-total=0
+total = 0
 for num in range(101):
     total = total + num
 print total
